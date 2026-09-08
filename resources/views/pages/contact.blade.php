0e061a8a34cc75b651da1a7b71c74d0f4f75375b
isaacokubi <63911280+isaacokubi@users.noreply.github.com>
2026-09-08 15:28:52 +0300

    Add Contact page

---
+++ resources/views/pages/contact.blade.php
@@ -0,0 +1,3 @@
+@extends('pages.layout')
+@section('title','Contact')
+@section('content')<h1>Contact the School</h1><p>School address, phone, email, location and enquiry form will be managed from the administration dashboard.</p>@endsection
