105af0400821c7a04b3eea4ec5f011abeabc0a64
isaacokubi <63911280+isaacokubi@users.noreply.github.com>
2026-09-08 15:44:30 +0300

    Restore Laravel 8 database seeder

---
+++ database/seeders/DatabaseSeeder.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+
+class DatabaseSeeder extends Seeder
+{
+    public function run()
+    {
+        // Application demo/initial data seeders can be called here.
+    }
+}
