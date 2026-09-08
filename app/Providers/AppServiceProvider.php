709b6a43431be22157d5bcd15737232870804640
isaacokubi <63911280+isaacokubi@users.noreply.github.com>
2026-09-08 15:40:48 +0300

    Restore Laravel 8 application service provider

---
+++ app/Providers/AppServiceProvider.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Providers;
+
+use Illuminate\Support\ServiceProvider;
+
+class AppServiceProvider extends ServiceProvider
+{
+    public function register()
+    {
+        // Application services may be registered here.
+    }
+
+    public function boot()
+    {
+        // Application bootstrapping may be registered here.
+    }
+}
