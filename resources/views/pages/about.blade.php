28b23343b9437f28c17596d77e11a8f84649adf0
isaacokubi <63911280+isaacokubi@users.noreply.github.com>
2026-09-08 15:28:36 +0300

    Add About page

---
+++ resources/views/pages/about.blade.php
@@ -0,0 +1,3 @@
+@extends('pages.layout')
+@section('title','About the School')
+@section('content')<h1>About the School</h1><p>School history, leadership, mission, vision and values will be managed from the administration dashboard.</p>@endsection
