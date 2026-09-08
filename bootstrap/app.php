0673565b9088890c3830a0a685f29172f308ef2d
isaacokubi <63911280+isaacokubi@users.noreply.github.com>
2026-09-08 15:37:20 +0300

    Fix Laravel 8 bootstrap compatibility for PHP 7.4

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -1,19 +1,32 @@
 <?php
 
-use Illuminate\Foundation\Application;
-use Illuminate\Foundation\Configuration\Exceptions;
-use Illuminate\Foundation\Configuration\Middleware;
+$app = new Illuminate\Foundation\Application(
+    $_ENV['APP_BASE_PATH'] ?? dirname(__DIR__)
+);
 
-return Application::configure(basePath: dirname(__DIR__))
-    ->withRouting(
-        web: __DIR__.'/../routes/web.php',
-        api: __DIR__.'/../routes/api.php',
-        commands: __DIR__.'/../routes/console.php',
-        health: '/up',
-    )
-    ->withMiddleware(function (Middleware $middleware): void {
-        // Application middleware can be registered here as features are added.
-    })
-    ->withExceptions(function (Exceptions $exceptions): void {
-        // Application exception configuration.
-    })->create();
+/*
+|--------------------------------------------------------------------------
+| Bind Important Interfaces
+|--------------------------------------------------------------------------
+|
+| Here we bind the interfaces into the container so they can be resolved
+| by the framework. You may change these bindings as needed.
+|
+*/
+
+$app->singleton(
+    Illuminate\Contracts\Http\Kernel::class,
+    App\Http\Kernel::class
+);
+
+$app->singleton(
+    Illuminate\Contracts\Console\Kernel::class,
+    App\Console\Kernel::class
+);
+
+$app->singleton(
+    Illuminate\Contracts\Debug\ExceptionHandler::class,
+    App\Exceptions\Handler::class
+);
+
+return $app;
