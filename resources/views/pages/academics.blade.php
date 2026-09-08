828f32f21f7882f9af7f303de40066a0f459674e
isaacokubi <63911280+isaacokubi@users.noreply.github.com>
2026-09-08 15:28:44 +0300

    Add Academics page

---
+++ resources/views/pages/academics.blade.php
@@ -0,0 +1,3 @@
+@extends('pages.layout')
+@section('title','Academics')
+@section('content')<h1>Academic Programmes</h1><p>Academic programmes, departments, facilities and staff information will be maintained by authorized school administrators.</p>@endsection
