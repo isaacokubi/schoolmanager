c63a75707e5f01b416d329488c1fce1e522cec2c
isaacokubi <63911280+isaacokubi@users.noreply.github.com>
2026-09-16 18:32:48 +0300

    Use polished admin pagination globally

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,7 @@
 
 namespace App\Providers;
 
+use Illuminate\Pagination\Paginator;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -13,6 +14,6 @@ class AppServiceProvider extends ServiceProvider
 
     public function boot()
     {
-        // Application bootstrapping may be registered here.
+        Paginator::defaultView('vendor.pagination.admin');
     }
 }
