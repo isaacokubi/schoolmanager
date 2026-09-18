0cf9f9e943aec6b2ce882a361834ff8bd7ad8605
isaacokubi <63911280+isaacokubi@users.noreply.github.com>
2026-09-18 21:33:38 +0300

    Add password reset status translations

---
+++ resources/lang/en/passwords.php
@@ -0,0 +1,9 @@
+<?php
+
+return [
+    'reset' => 'Your password has been reset successfully.',
+    'sent' => 'A password reset link has been sent to your email address.',
+    'throttled' => 'Please wait before trying again.',
+    'token' => 'This password reset link is invalid or has expired.',
+    'user' => 'We could not find an account registered with that email address.',
+];
