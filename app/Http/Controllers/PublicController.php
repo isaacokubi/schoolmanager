7c626daa3ad27b1535326b6dbb6e049564807d62
isaacokubi <63911280+isaacokubi@users.noreply.github.com>
2026-09-18 21:06:31 +0300

    Fix malformed PublicController support method

--- app/Http/Controllers/PublicController.php
+++ app/Http/Controllers/PublicController.php
@@ -73,7 +73,12 @@ class PublicController extends Controller
         ]);
     }
 
-    public function support() { return app(\App\Http\Controllers\AngelHomeParityController::class)->support(); }\n\n    public function contact()
+    public function support()
+    {
+        return app(AngelHomeParityController::class)->support();
+    }
+
+    public function contact()
     {
         return view('pages.contact', [
             'settings' => $this->settings(),
